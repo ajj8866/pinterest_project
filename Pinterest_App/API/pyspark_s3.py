72dd71f2ec22ed5d34752b9e4f3f1e8b691f7880
Ghulam  Jatoi <aj8@Ghulams-MacBook-Pro.local>
2022-10-13 21:59:35 +0100

    Wrote up script for cleaning follower_count column

--- Pinterest_App/API/pyspark_s3.py
+++ Pinterest_App/API/pyspark_s3.py
@@ -1,9 +1,11 @@
 import imp
 from click import option
+from pip import main
 import py
 from pyspark.sql.types import StructField, StringType, IntegerType, StructType
 import multiprocessing
 import pyspark
+from pyspark.sql.functions import col, when, regexp_replace
 import requests
 from time import sleep
 import random
@@ -13,6 +15,8 @@ import json
 import sqlalchemy
 import time
 from threading import Event, Thread
+
+from torch import StringType
 from User_Emulation.user_posting_emulation import run_infinite_post_data_loop, AWSDBConnector
 import os
 
@@ -22,9 +26,9 @@ class PySparkIntegrations:
         self.aws_id= os.environ.get('AWS_ACCESS_ID')
         self.aws_secret= os.environ.get('AWS_SECRET')
         self.aws_client= boto3.client('s3', aws_access_key_id= self.aws_id, region_name=region,aws_secret_access_key=self.aws_secret)
-        self.schema= [StructField('category', StringType, nullable=True), StructField('unique_id', StringType, nullable=False), 
-            StructField('title', StringType, nullable=True), StructField('description', StringType, nullable=True), StructField('follower_count', StringType, nullable=True), 
-            StructField('tag_list', StringType, nullable=True), StructField('is_image_or_video', StringType, nullable=True), StructField('image_src', StringType, nullable=True),StructField('downloaded', IntegerType), StructField('save_location', StringType, nullable=True)]
+        self.schema= StructType([StructField('category', StringType(), nullable=True), StructField('index', StringType(),nullable=True), StructField('unique_id', StringType(), nullable=False), 
+            StructField('title', StringType(), nullable=True), StructField('description', StringType(), nullable=True), StructField('follower_count', StringType(), nullable=True), 
+            StructField('tag_list', StringType(), nullable=True), StructField('is_image_or_video', StringType(), nullable=True), StructField('image_src', StringType(), nullable=True),StructField('downloaded', IntegerType()), StructField('save_location', StringType(), nullable=True)])
 
     def get_s3(self, hadoop_aws_version= '3.3.4'):
         # Setting configuration parameters for spark session
@@ -43,9 +47,21 @@ class PySparkIntegrations:
         sc._jsc.hadoopConfiguration().set('fs.s3a.secret.key', self.aws_secret)
         main_df= self.spark_session.createDataFrame(sc.parallelize([]), schema=self.schema)
         for csv_file in self.aws_client.list_objects_v2(Bucket=self.bucket_name)['Contents']['Key']:
-            temp_df= self.spark_session.read.csv(f's3a:{self.bucket_name}/{csv_file}', schema=self.schema)
+            temp_df= self.spark_session.read.csv(f's3a:{self.bucket_name}/{csv_file}', header=True, inferSchema=True)
             main_df= main_df.union(temp_df)
         
+        sql_view= main_df.createOrReplaceTempView('pinterest_table')
+        sql_query= self.spark_session.sql('SELECT DISTINCT SUBSTRING(follower_count, -1) FROM pinterest_table')
+        sql_query.show()
+        main_df= main_df.withColumn('follower_count', \
+            when(main_df.follower_count.endswith('k'), regexp_replace(main_df.follower_count, 'k', '000')) \
+            .when(main_df.follower_count.endswith('M'), regexp_replace(main_df.follower_count, 'M', '000000')) \
+            .when(main_df.follower_count.endswith('r'), regexp_replace(main_df.follower_count, r'.*', '0')) \
+            .when(main_df.follower_count.endswith('"'), regexp_replace(main_df.follower_count, r'.*', '0')).otherwise(main_df.follower_count))
+
+        main_df.show()
+
+        main_df= main_df.withColumn('follower_count', main_df.follower_count.cast('int'))
 
     
     def get_kafka(self, topic_name='pinterest_topic'):
